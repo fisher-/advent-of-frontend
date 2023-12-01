aa7c9600a6385b0beae1f244119b5aed3021b23e
fisher- <tomasz.m.rybak@gmail.com>
2023-12-01 23:03:39 +0100

    2023-12-01

---
+++ tasks/2023-12-01/index.ts
@@ -0,0 +1,25 @@
+export class GiftRegistry {
+  registry = new Map<number, string[]>();
+
+  addGift(childId: number, gift: string): void {
+    const childGifts = this.registry.get(childId) || [];
+    !childGifts.includes(gift) && childGifts.push(gift);
+    this.registry.set(childId, childGifts);
+  }
+
+  removeGift(childId: number, gift: string): void {
+    const childGifts = this.registry.get(childId);
+    if (!childGifts) {
+      throw new Error("Child not found");
+    }
+    if (!childGifts.includes(gift)) {
+      throw new Error("Gift not found");
+    }
+    childGifts.splice(childGifts.indexOf(gift), 1);
+    this.registry.set(childId, childGifts);
+  }
+
+  getGiftsForChild(childId: number): string[] | undefined {
+    return this.registry.get(childId);
+  }
+}
